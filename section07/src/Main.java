fd10d2b25b36b360af17aacb6bee1f6feae37efe
rodell <ryanodell2668@gmail.com>
2023-01-07 16:17:55 -0500

    7.83 | assigning multiple objects to same class

--- section07/src/Main.java
+++ section07/src/Main.java
@@ -8,6 +8,16 @@ public class Main {
         // both state and behavior are a part of this 'car' class -> very powerful
         Car car = new Car("Porsche", "Carrera", "black", 2, true);  // "new Car()" -> assigning object reference to car
 
+        // Another reference created but points to the same object in memory
+        Car anotherCar = car;
+
         System.out.println("make = " + car.getMake());  // make = Porsche
+        System.out.println(car.getMake());  // Porsche
+
+        // Change object in memory
+        car.setMake("tesla");
+        System.out.println(car.getMake());  // tesla
+        System.out.println(anotherCar.getMake());  // tesla
+
     }
 }
